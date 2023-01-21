e0c3446c1d7b5a2c9862756623e22c52dd0c9edf
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2023-01-20 20:49:15 -0700

    fix: let it be imported

--- source/index.js
+++
@@ -1,2 +0,0 @@
-export { default as nullToUndefined } from './null-to-undefined.js';
-export { default as undefinedToNull } from './undefined-to-null.js';
--- tsup.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsup';
-export default defineConfig({
-    clean: true,
-    entry: ['source/index.ts'],
-    format: ['cjs', 'esm'],
-    outDir: 'build',
-    sourcemap: true,
-});
